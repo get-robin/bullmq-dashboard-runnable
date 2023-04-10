5c076bdea6723caab696b93a95a1190e7bb014fc
Adam Smith <adamtysonsmith@gmail.com>
2023-04-10 13:10:20 -0700

    stage script

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 const { createBullBoard } = require("@bull-board/api");
 const { BullMQAdapter } = require("@bull-board/api/bullMQAdapter");
 const { ExpressAdapter } = require("@bull-board/express");
@@ -19,41 +18,50 @@ function myParseInt(value) {
 }
 
 program
+  .version("0.0.1")
   .showHelpAfterError()
   .option("-P, --port <port>", "Port to run UI", myParseInt, 3000)
   .option("--redis-port <port>", "Redis port", myParseInt, 6379)
   .option("--redis-host <host>", "Redis host", "localhost")
+  .option("--redis-user <user>", "Redis user", "bob")
   .option("--redis-password <password>", "Redis password", undefined)
   .option("--prefix <prefix>", "BullMQ prefix", undefined)
-  .argument("<names>", "comma separated queue names", split)
-  .action((names, opts) => {
-    const redisOptions = {
-      port: opts.redisPort,
-      host: opts.redisHost,
-      password: opts.redisPassword,
-    };
+  .argument("<queue names>", "comma separated queue names", split)
+  .parse(process.argv);
 
-    const run = async () => {
-      const queues = names.map(
-        (name) =>
-          new BullMQAdapter(new Queue(name, { ...(opts.prefix ? {prefix: opts.prefix}: {}),  connection: redisOptions }))
-      );
+const names = program.args
+const opts = program.opts()
 
-      const app = express();
 
-      const serverAdapter = new ExpressAdapter();
-      serverAdapter.setBasePath("/");
+if (!opts) {
+  process.exit()
+}
 
-      createBullBoard({ queues, serverAdapter });
+const redisOptions = {
+  username: opts.redisUser,
+  port: opts.redisPort,
+  host: opts.redisHost,
+  password: opts.redisPassword,
+};
 
-      app.use("/", serverAdapter.getRouter());
+const run = async () => {
+  const queues = names.map(
+    (name) =>
+      new BullMQAdapter(new Queue(name, { ...(opts.prefix ? {prefix: opts.prefix}: {}),  connection: redisOptions }))
+  );
 
-      app.listen(opts.port, () => {
-        console.log(`For the UI, open http://localhost:${opts.port}/`);
-      });
-    };
+  const app = express();
 
-    run().catch((e) => console.error(e));
+  const serverAdapter = new ExpressAdapter();
+  serverAdapter.setBasePath("/");
+
+  createBullBoard({ queues, serverAdapter });
+
+  app.use("/", serverAdapter.getRouter());
+
+  app.listen(opts.port, () => {
+    console.log(`For the UI, open http://localhost:${opts.port}/`);
   });
+};
 
-program.parse();
+run().catch((e) => console.error(e));
---
+++ scripts/stage.sh
@@ -0,0 +1,14 @@
+node index.js \
+  bull:twilio:send:utc-4 \
+  bull:twilio:send:utc-5 \
+  bull:twilio:send:utc-6 \
+  bull:twilio:send:utc-7 \
+  bull:twilio:send:utc-8 \
+  bull:twilio:send:utc-9 \
+  bull:twilio:send:utc-10 \
+  --port 3369 \
+  --redis-user stage \
+  --redis-host redis-13212.c273.us-east-1-2.ec2.cloud.redislabs.com \
+  --redis-port 13212 \
+  --redis-user stage \
+  --redis-password "<ENTER_PASSWORD_IN_QUOTES>" 
